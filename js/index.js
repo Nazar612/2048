b0568235e79004929e7550dafc283ea752466373
Nazar612 <nazarstovba300@gmail.com>
2026-08-04 14:01:09 +0200

    2048

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
+const elements = {
+    boxes: document.querySelectorAll(".box")
+}
+
+console.log(elements.boxes);
+
 let matrixCreate = new Array(4).fill().map(() => new Array(4).fill(""));
+// console.log(matrixCreate);
+let i = 0;
+let boxesI = 0;
+for(let row = 0; row < 4; row++) {
+    for(let col = 0; col < 4; col++) {
+        i++
+        matrixCreate[row][col] = i;
+
+        elements.boxes[boxesI].textContent = matrixCreate[row][col];
+
+        boxesI++
+    }
+}
+
 console.log(matrixCreate);
